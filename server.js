82364aeb139171baaba6c4d25189214e1d1ef840
tb0se <rachiditbose@gmail.com>
2021-02-10 23:52:32 +0200

    add database url

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      
     }
 });
+// user : 'postgres',
+//       password : 'test',
+//       database : 'smart-brain'
 
 const app = express();
 app.use(express.json());
